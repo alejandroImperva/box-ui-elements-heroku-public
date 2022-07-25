8abbf8b928f6ec57d33c7f4f3813d4c196f42439
alejandroImperva <96626003+alejandroImperva@users.noreply.github.com>
2022-07-25 22:35:51 +0100

    Update ContentExplorer.js

--- src/components/ContentExplorer.js
+++ src/components/ContentExplorer.js
@@ -1,48 +1,57 @@
-export default ({ folderId, tokenChild }) => {
-  const [rootFolderId, setRootFolderId] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
+import React, { useState, useEffect } from 'react';
 
-  if (tokenChild) {
-      setRootFolderId(folderId);
-      console.log('Loading UI Element...');
+import axios from 'axios';
+import { ContentExplorer } from 'box-ui-elements';
+import { ScaleLoader } from 'react-spinners';
+import { THEME_COLOR, EXPRESS_SERVER_HOST } from '../Constants';
 
-      return (
-              <div className="elements">
-                      <ContentExplorer
-                          logoUrl={""}
-                          rootFolderId={rootFolderId}
-                          token={tokenChild}
-                          language={"en_US"}
-                          contentPreviewProps={{
-                              showAnnotations: true,
-                              contentSidebarProps: {
-                                  detailsSidebarProps: {
-                                      hasProperties: true,
-                                      hasNotices: true,
-                                      hasAccessStats: true,
-                                      hasClassification: true,
-                                      hasRetentionPolicy: true,
-                                  },
-                                  hasActivityFeed: true,
-                                  hasMetadata: true,
-                                  // hasSkills: true,
-                                  hasVersions: true
-                              }
-                          }}
-                      />
-              </div>
-      );
-  }
-  else {
-      return(
-              <div className="elements">
-                  <div className="spinner">
-                      <ScaleLoader 
-                          color={THEME_COLOR}
-                          loading={isLoading}
-                      />
-                  </div>                
-              </div>
-      );
-  }
+
+export default ({ folderId, tokenChild  }) => {
+    const [token, setToken] = useState(null);
+    const [rootFolderId, setRootFolderId] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    if(tokenChild) {
+        console.log('Loading UI Element...');
+
+        return (
+                <div className="elements">
+                        <ContentExplorer
+                            logoUrl={""}
+                            rootFolderId={rootFolderId}
+                            token={token}
+                            language={"en_US"}
+                            contentPreviewProps={{
+                                showAnnotations: true,
+                                contentSidebarProps: {
+                                    detailsSidebarProps: {
+                                        hasProperties: true,
+                                        hasNotices: true,
+                                        hasAccessStats: true,
+                                        hasClassification: true,
+                                        hasRetentionPolicy: true,
+                                    },
+                                    hasActivityFeed: true,
+                                    hasMetadata: true,
+                                    // hasSkills: true,
+                                    hasVersions: true
+                                }
+                            }}
+                        />
+                </div>
+        );
+    }
+    else {
+        return(
+                <div className="elements">
+                    <div className="spinner">
+                        <ScaleLoader 
+                            color={THEME_COLOR}
+                            loading={isLoading}
+                        />
+                    </div>                
+                </div>
+        );
+    }
 };
