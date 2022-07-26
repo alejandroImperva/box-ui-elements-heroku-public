56b7f14245866e07a2be6d2c58f73ce82dbd3d89
alejandroImperva <96626003+alejandroImperva@users.noreply.github.com>
2022-07-26 20:41:41 +0100

    Update ContentExplorer.js

--- src/components/ContentExplorer.js
+++ src/components/ContentExplorer.js
@@ -1,45 +1,53 @@
 import React, { useState, useEffect } from 'react';
-
+// import { useSearchParams } from "react-router-dom";
 import axios from 'axios';
 import { ContentExplorer } from 'box-ui-elements';
 import { ScaleLoader } from 'react-spinners';
 import { THEME_COLOR, EXPRESS_SERVER_HOST } from '../Constants';
 
 
-export default ({ folderId, tokenChild  }) => {
+export default ({ userId, folderId  }) => {
     const [token, setToken] = useState(null);
-    const [rootFolderId, setRootFolderId] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
-
-
-    if (tokenChild) {
-       setRootFolderId(folderId);
-        console.log('Loading UI Element...');
-
+    const [rootFolderId, setRootFolderId] = useState(0);
+    
+    useEffect(() => {
+        const fetchToken = async () => {
+            console.log('Found userId: ', userId)
+            setIsLoading(true);
+            const result = await axios.get(`${EXPRESS_SERVER_HOST}/box/explorer/token-downscope/${userId}`);
+            setToken(result.data.accessToken);
+            setIsLoading(false);
+        }
+        fetchToken();
+    }, []);
+    
+    if (token) {
+      setRootFolderId(folderId);
         return (
                 <div className="elements">
-                        <ContentExplorer
-                            logoUrl={""}
-                            rootFolderId={rootFolderId}
-                            token={tokenChild}
-                            language={"en_US"}
-                            contentPreviewProps={{
-                                showAnnotations: true,
-                                contentSidebarProps: {
-                                    detailsSidebarProps: {
-                                        hasProperties: true,
-                                        hasNotices: true,
-                                        hasAccessStats: true,
-                                        hasClassification: true,
-                                        hasRetentionPolicy: true,
-                                    },
-                                    hasActivityFeed: true,
-                                    hasMetadata: true,
-                                    // hasSkills: true,
-                                    hasVersions: true
-                                }
-                            }}
-                        />
+                <ContentExplorer
+                    logoUrl={""}
+                    rootFolderId={rootFolderId}
+                    token={token}
+                    language={"en_US"}
+                    contentPreviewProps={{
+                        showAnnotations: true,
+                        contentSidebarProps: {
+                            detailsSidebarProps: {
+                                hasProperties: true,
+                                hasNotices: true,
+                                hasAccessStats: true,
+                                hasClassification: true,
+                                hasRetentionPolicy: true,
+                                hasVersions: true,
+                            },
+                            hasActivityFeed: true,
+                            hasMetadata: true,
+                            hasSkills: true
+                        }
+                    }}
+                />
                 </div>
         );
     }
